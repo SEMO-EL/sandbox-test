bbc02b8313d3c8d9c86a8616804addbe66208be5
SEMO <92227654+SEMO-EL@users.noreply.github.com>
2026-01-20 20:27:17 +0000

    Add 'scale' option to AppState mode property

--- core/state.js
+++ core/state.js
@@ -11,7 +11,7 @@
 
 /**
  * @typedef {Object} AppState
- * @property {"rotate"|"move"|"orbit"} mode
+ * @property {"rotate"|"move"|"orbit"|"scale"} mode
  * @property {AxisState} axis
  * @property {number} snapDeg
  * @property {boolean} showGrid
